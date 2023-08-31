cd9ab3c3082bf4382d618efcccc5829f89494bd9
XimeonLeo <ximeonleo@gmail.com>
2023-08-31 11:33:15 +0100

    preorder transversal

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_preorder - traverse through a tree in preorderly
+ *
+ * @tree: a pointer to the root node
+ * @func: a function pointer tne calls each node. it takes the value
+ * in the node as a parameter
+ */
+
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree && func)
+	{
+		func(tree->n);
+		binary_tree_preorder(tree->left, func);
+		binary_tree_preorder(tree->right, func);
+	}
+}
