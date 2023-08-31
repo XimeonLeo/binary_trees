00a41b5139f06b88665ad5af5c9b709242c0a46d
XimeonLeo <ximeonleo@gmail.com>
2023-08-31 11:10:00 +0100

    is it a root node

---
+++ 5-binary_tree_is_root.c
@@ -0,0 +1,16 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_root - checks if a node has no parent(root)
+ *
+ * @node: the node to check
+ *
+ * Return: 1 for a root node or 0 if not
+ */
+
+int binary_tree_is_root(const binary_tree_t *node)
+{
+	if (!node || !node->parent)
+		return (1);
+	return (0);
+}
